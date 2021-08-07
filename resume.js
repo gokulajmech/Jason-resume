1c7605c937be6896c83edc8f50d5d32355c25f7e
gokulajmech <85476372+gokulajmech@users.noreply.github.com>
2021-08-07 22:23:07 +0530

    Add files via upload

---
+++ resume.js
@@ -0,0 +1,11 @@
+let resume={
+    name:'gokulrajan',
+    mobNo:'8760395074',
+    mailId:'gokulraj1414@gmail.com',
+    address:'2/44a,Mattuvelampatty,Rasipuram,Namakkal(DT)Pin-637505',
+    language_known:'English,Tamil,Japanese(N4)',
+    software_skills:'c,javaScript,html,css',
+    passed_out_year:'2021',
+    Experience:'worked as a test engineer in (ETL TESTING)'
+};
+console.log(resume);
